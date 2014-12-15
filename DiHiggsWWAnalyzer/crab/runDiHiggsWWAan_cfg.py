c6c908b4cb57b4070d00ac285a0f96444ca452ed
tahuang1991 <taohuang@physics.tamu.edu>
2014-12-15 09:33:54 -0600

    add crab dir

--- DiHiggsWWAnalyzer/crab/runDiHiggsWWAan_cfg.py
+++ DiHiggsWWAnalyzer/crab/runDiHiggsWWAan_cfg.py
@@ -6,13 +6,20 @@ process.load("FWCore.MessageService.MessageLogger_cfi")
 
 process.maxEvents = cms.untracked.PSet( input = cms.untracked.int32(1) )
 
-
+#input files
 process.source = cms.Source("PoolSource",
     # replace 'myfile.root' with the source file you want to use
     fileNames = cms.untracked.vstring(
         'file:/uscms_data/d3/tahuang/CMSSW_6_2_0_SLHC16/src/GEMCode/SimMuL1/debug/PU0_10k_Pt20_out_L1.root'
     )
 )
+
+#output files
+process.TFileService = cms.Service("TFileService",
+	    fileName = cms.string("out_Ana.root")
+)
+
+
 
 process.DiHiggsWWAna = cms.EDAnalyzer('DiHiggsWWAnalyzer')
 process.dump=cms.EDAnalyzer('EventContentAnalyzer')
